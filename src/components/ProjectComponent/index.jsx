c54b19c66cf2048dd365b489745cfce54669825a
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-06-14 02:57:39 -0300

    finalizando responsividade

--- src/components/ProjectComponent/index.jsx
+++ src/components/ProjectComponent/index.jsx
@@ -2,19 +2,20 @@ import projects from "../../projects"
 
 export const ProjectsComponent = () => {
     return(
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 w-full max-w-160 h-full mt-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 w-full h-full mt-10">
             {projects.map((projeto) => (
-                <section key={projeto.id}>
-                    <article className="border flex items-center justify-center flex-col border-white rounded-md p-4">
+                <section key={projeto.id} className="hover:scale-110 duration-300 transition-all ease-in-out">
+                    <article className="border flex items-center justify-center flex-col border-white rounded-xl p-4">
+                         <h1 className="text-white text-xl text-center mt-5 mb-5">{projeto.projectName}</h1>
                         <img 
                         src={projeto.projectImage} 
                         alt={projeto.projectName}
-                        className="h-30 object-cover w-70 rounded-md"
+                        className="lg:h-30 h-80 object-cover lg:w-70  px-2 rounded-lg"
                         />
-                        <h1 className="text-white text-xl text-center">{projeto.projectName}</h1>
-                        <p className="text-white line-clamp-2 max-w-70 text-center">{projeto.description}</p>
+                       
+                        <p className="text-white lg:line-clamp-2 lg:max-w-70 text-center mt-5">{projeto.description}</p>
 
-                        <button className="bg-white flex items-center w-full justify-center rounded-md font-medium text-lg">
+                        <button className="bg-white flex items-center w-full justify-center rounded-md font-medium text-lg mt-10">
                                 <a target="_blank" href={projeto.link}>
                                 acessar
                             </a>
